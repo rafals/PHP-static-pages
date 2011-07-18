03ac1470eb1d5d4b41dfb6fd07f46450178cb2b5
Rafał Sobota <ravsobota@gmail.com>
2011-07-18 13:24:55 -0700

    page default located at root domain

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 // strona umieszczona na http://a.com/
-//$request = $_SERVER['REQUEST_URI'];
+$request = $_SERVER['REQUEST_URI'];
 
 // strona umieszczona na http://a.com/b/c/d/
-$request = str_replace('/b/c/d/', '/', $_SERVER['REQUEST_URI']);
+//$request = str_replace('/b/c/d/', '/', $_SERVER['REQUEST_URI']);
 
 $params = array();
 $params_num = 0;
